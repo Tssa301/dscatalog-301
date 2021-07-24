088a061c2c9d7fb90369e05a8fa51638fb29d739
Tiago Ssa <tiago.silva301@gmail.com>
2021-07-24 01:39:32 +0100

    Backend project created

---
+++ backend/src/main/java/com/tssa301/dscatalog/DscatalogApplication.java
@@ -0,0 +1,13 @@
+package com.tssa301.dscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscatalogApplication.class, args);
+	}
+
+}
