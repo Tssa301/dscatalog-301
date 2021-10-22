785965caea21af667969990b74902772487ce841
Tiago Ssa <tiago.silva301@gmail.com>
2021-10-22 21:22:57 +0100

    Navbar styling

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,10 +1,16 @@
+import Navbar from 'components/Navbar';
 import './assets/styles/custom.scss';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <h1>Hello DSCatalog</h1>
+    <>
+      <Navbar/>
+      <h1>Hello DSCatalog</h1>
+    </>
+    
   );
 }
 
 export default App;
+ 
