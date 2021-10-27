63257488b56d62168070d27a2f93f15f7b61d810
Tiago Ssa <tiago.silva301@gmail.com>
2021-10-27 22:11:29 +0100

    ProductDetails layout mobilefirst

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -1,8 +1,36 @@
-const ProductDetails = () => {
+import {ReactComponent as ArrowIcon} from 'assets/images/arrow.svg';
+import ProductPrice from 'components/ProductPrice';
 
-    return (
-        <h1>Product Details</h1>
-    );
-}
+import './styles.css'
+
+const ProductDetails = () => {
+  return (
+    <div className="product-details-container">
+      <div className=" base-card product-details-card">
+        <div className="goback-container">
+          <ArrowIcon/>
+          <h2>Back</h2>
+        </div>
+        <div className="row">
+          <div className="col-xl-6">
+            <div className="img-container">
+              <img src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg" alt="Product name" />
+            </div>
+            <div className="name-price-container">
+              <h1>Product name</h1>
+              <ProductPrice price={2345.67}/>
+            </div>
+          </div>
+          <div className="col-xl-6">
+            <div className="description-container">
+              <h2>Product description</h2>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et, vel.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ProductDetails;
