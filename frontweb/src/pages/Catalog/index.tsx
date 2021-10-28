18a8bf8f0814277651a182603bfe8745175c8afe
Tiago Ssa <tiago.silva301@gmail.com>
2021-10-28 22:47:31 +0100

    Pagination styling

--- frontweb/src/pages/Catalog/index.tsx
+++ frontweb/src/pages/Catalog/index.tsx
@@ -1,3 +1,4 @@
+import Pagination from 'components/Pagination';
 import ProductCard from 'components/ProductCard';
 import { Link } from 'react-router-dom';
 import { Product } from 'types/product';
@@ -71,6 +72,9 @@ const Catalog = () => {
             <ProductCard product={product} />
           </Link>
         </div>
+      </div>
+      <div className="row">
+        <Pagination/>
       </div>
     </div>
   );
