8deefc8440cd13008c90219025ae76b6b7c982ef
Tiago Ssa <tiago.silva301@gmail.com>
2021-12-16 00:05:16 +0000

    useState, useEffect and useParams

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -1,10 +1,30 @@
 import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
+import axios from 'axios';
 import ProductPrice from 'components/ProductPrice';
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { Product } from 'types/product';
+import { BASE_URL } from 'util/requests';
 
 import './styles.css';
 
+type UrlParams = {
+  productId: string
+}
+
 const ProductDetails = () => {
+
+  const {productId} = useParams<UrlParams>();
+
+  const [product, setProduct] = useState<Product>();
+    
+    useEffect(() => {
+      axios.get(`${BASE_URL}/products/${productId}`)
+      .then(response => {
+        setProduct(response.data)
+      });
+    }, [productId]);
+
   return (
     <div className="product-details-container">
       <div className=" base-card product-details-card">
@@ -18,21 +38,20 @@ const ProductDetails = () => {
           <div className="col-xl-6">
             <div className="img-container">
               <img
-                src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg"
-                alt="Product name"
+                src={product?.imgUrl}
+                alt={product?.name}
               />
             </div>
             <div className="name-price-container">
-              <h1>Product name</h1>
-              <ProductPrice price={2345.67} />
+              <h1>{product?.name}</h1>
+              {product && <ProductPrice price={product?.price} />}
             </div>
           </div>
           <div className="col-xl-6">
             <div className="description-container">
               <h2>Product description</h2>
               <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et,
-                vel.
+                {product?.description}
               </p>
             </div>
           </div>
