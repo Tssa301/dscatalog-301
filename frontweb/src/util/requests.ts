9e880f39099ac24078e56969f8709e809ead1cac
Tiago Ssa <tiago.silva301@gmail.com>
2022-01-08 19:20:16 +0000

    Check user authenticated

--- frontweb/src/util/requests.ts
+++ frontweb/src/util/requests.ts
@@ -1,6 +1,16 @@
 import axios, { AxiosRequestConfig } from "axios";
+import { type } from "os";
 import qs from "qs";
 import history from './history'
+import jwtDecode from 'jwt-decode';
+
+type Role = 'ROLE_OPERATOR' | 'ROLE_ADMIN'
+
+type TokenData = {
+  exp: number,
+  user_name: string,
+  authorities: Role[];
+}
 
 type LoginResponse = {
     access_token: string,
@@ -78,3 +88,17 @@ axios.interceptors.response.use(function (response) {
     }
     return Promise.reject(error);
   });
+
+  export const getTokenData = () : TokenData | undefined => {
+      try{
+          return jwtDecode(getAuthData().access_token) as TokenData;
+      }
+      catch(error) {
+          return undefined;
+      }
+  }
+
+  export const isAuthenticated = () : boolean => {
+      const tokenData = getTokenData();
+      return (tokenData && tokenData.exp * 1000 > Date.now()) ? true : false;
+  }
