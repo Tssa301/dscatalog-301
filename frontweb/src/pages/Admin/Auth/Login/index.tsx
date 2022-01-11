f3a687c24ac02a8623334dbd2e0ee531c830c465
Tiago Ssa <tiago.silva301@gmail.com>
2022-01-11 00:31:26 +0000

    Organizing utils

--- frontweb/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -3,7 +3,9 @@ import ButtonIcon from 'components/ButtonIcon';
 import { useContext, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { Link, useHistory, useLocation } from 'react-router-dom';
-import { getTokenData, requestBackendLogin, saveAuthData } from 'util/requests';
+import { getTokenData } from 'util/auth';
+import { requestBackendLogin } from 'util/requests';
+import { saveAuthData } from 'util/storage';
 
 import './styles.css';
 
