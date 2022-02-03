917ff6d6b34aed2776e667c214290aa7cc902616
Tiago Ssa <tiago.silva301@gmail.com>
2022-02-03 05:48:07 +0000

    Products crud routes

---
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -0,0 +1,9 @@
+
+const Form = () => {
+
+    return(
+        <h3>Form screen</h3>
+    );
+}
+
+export default Form;
---
+++ frontweb/src/pages/Admin/Products/List/index.tsx
@@ -0,0 +1,10 @@
+
+const List = () => {
+
+    return(
+
+        <h3>List screen</h3>
+    );
+}
+
+export default List;
