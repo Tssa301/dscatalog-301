2cd4cc0f8f89472ec692b5396e511ed50237a6f4
Tiago Ssa <tiago.silva301@gmail.com>
2022-02-08 06:41:22 +0000

    Product crud form HTML

--- frontweb/src/pages/Admin/Products/Form/index.tsx
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -1,9 +1,33 @@
+import './styles.css';
 
 const Form = () => {
-
-    return(
-        <h3>Form screen</h3>
-    );
-}
+  return (
+    <div className="product-crud-container">
+      <div className="base-card product-crud-form-card">
+        <h1 className="product-crud-form-title">PRODUCT DATA</h1>
+        <form action="">
+          <div className="row">
+            <div className="col-lg-6">
+              <input type=" text" className="form-control base-input" />
+              <input type=" text" className="form-control base-input" />
+              <input type=" text" className="form-control base-input" />
+            </div>
+            <div className="col-lg-6">
+              <textarea
+                name=""
+                rows={10}
+                className="form-control base-input"
+              ></textarea>
+            </div>
+          </div>
+          <div>
+            <button className="btn btn-outline-danger">CANCEL</button>
+            <button className="btn btn-primary">SAVE</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Form;
