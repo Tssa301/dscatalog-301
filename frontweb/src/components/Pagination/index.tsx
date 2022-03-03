303d9840155a34c9d1adddd61cb72ef079b377f4
Tiago Ssa <tiago.silva301@gmail.com>
2022-03-03 06:50:20 +0000

    Pagination layout

--- frontweb/src/components/Pagination/index.tsx
+++ frontweb/src/components/Pagination/index.tsx
@@ -1,18 +1,24 @@
 import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
+import ReactPaginate from 'react-paginate';
 
-import './styles.css'
+import './styles.css';
 
 const Pagination = () => {
   return (
-    <div className="pagination-container">
-      <ArrowIcon className="arrow-previous arrow-inactive" />
-      <div className="pagination-item active">1</div>
-      <div className="pagination-item">2</div>
-      <div className="pagination-item">3</div>
-      <div className="pagination-item">...</div>
-      <div className="pagination-item">10</div>
-      <ArrowIcon className="arrow-next arrow-active" />
-    </div>
+    <ReactPaginate
+      pageCount={10}
+      pageRangeDisplayed={3}
+      marginPagesDisplayed={1}
+      containerClassName="pagination-container"
+      pageLinkClassName="pagination-item"
+      breakClassName="pagination-item"
+      previousClassName="arrow-previous"
+      nextClassName="arrow-next"
+      activeLinkClassName="pagination-link-active"
+      disabledClassName="arrow-inactive"
+      previousLabel={<ArrowIcon />}
+      nextLabel={<ArrowIcon />}
+    />
   );
 };
 
--- frontweb/src/pages/Admin/NavBar/index.tsx
+++ frontweb/src/pages/Admin/NavBar/index.tsx
@@ -5,7 +5,7 @@ import './styles.css';
 const NavBar = () => {
   return (
     <nav className="admin-nav-container">
-      <ul>
+      <ul className="admin-nav-items-container">
         <li>
           <NavLink to="/admin/products" className="admin-nav-item">
             <p>Products</p>
